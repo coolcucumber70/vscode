06052fcd6dbbc9ce2ad2f10f565c2d68ea54c685
coolcucumber70 <794746725@qq.com>
2022-09-13 15:49:19 +0800

    2022.9.13 670最大交换

---
+++ 670.最大交换.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=670 lang=cpp
+ *
+ * [670] 最大交换
+ */
+
+// @lc code=start
+using namespace std;
+#include<iostream>
+#include<string>
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string numarry= to_string(num);
+        int n=numarry.size();
+        for(int i=0;i<n;i++){
+            int k=i;
+            for(int j=i+1;j<n;j++){
+                if ((numarry[j]>=numarry[k])){
+                    
+                    k=j;
+                }
+            }
+            if((k!=i)&&(numarry[k]!=numarry[i])){
+                swap(numarry[i],numarry[k]);
+                return stoi(numarry);
+                
+            }
+        }
+        return num;
+        
+    }
+};
+// @lc code=end
