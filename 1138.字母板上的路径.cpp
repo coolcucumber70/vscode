e59b0e0b38b41db18b48dcc563ca533ec15dbe0e
coolcucumber70 <794746725@qq.com>
2023-02-12 09:11:43 +0800

    1138.字母板上的路径

---
+++ 1138.字母板上的路径.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=1138 lang=cpp
+ *
+ * [1138] 字母板上的路径
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string alphabetBoardPath(string target) {
+        int cx = 0, cy = 0;
+        string res;
+        for (char c : target) {
+            int nx = (c - 'a') / 5;
+            int ny = (c - 'a') % 5;
+            if (nx < cx) {
+                res.append(cx - nx, 'U');
+            }
+            if (ny < cy) {
+                res.append(cy - ny, 'L');
+            } 
+            if (nx > cx) {
+                res.append(nx - cx, 'D');
+            }
+            if (ny > cy) {
+                res.append(ny - cy, 'R');
+            }  
+            res.push_back('!');
+            cx = nx;
+            cy = ny;
+        }
+        return res;
+    }
+};
+// @lc code=end
+
