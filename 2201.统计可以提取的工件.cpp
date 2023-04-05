537b99bf5497c83c51bd815e14cc5cbb1f087c76
coolcucumber70 <794746725@qq.com>
2023-04-05 12:04:22 +0800

    2427

---
+++ 2201.统计可以提取的工件.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=2201 lang=cpp
+ *
+ * [2201] 统计可以提取的工件
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int digArtifacts(int n, vector<vector<int>>& artifacts, vector<vector<int>>& dig) {
+        
+    }
+};
+// @lc code=end
+
